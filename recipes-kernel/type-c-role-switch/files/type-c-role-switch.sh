b623298a3cc5ed865a9a3fad154b926470b7d515
Bernardo Garza <bernardo.garza@hlioengineering.com>
2022-01-27 14:27:07 -0600

    Updated the script to properly change the state of the cc pins to have them properly set when the display is acting as either a peripheral or a host.

--- recipes-kernel/type-c-role-switch/files/type-c-role-switch.sh
+++ recipes-kernel/type-c-role-switch/files/type-c-role-switch.sh
@@ -2,6 +2,9 @@ set -e
 
 command=$1
 role_file_path="/sys/devices/platform/soc/49000000.usb-otg/usb_role/49000000.usb-otg-role-switch/role"
+base_gpio_path="/sys/class/gpio"
+cc_rd_en_pin="87"
+cc_rp_en_pin="89"
 
 help_diag() {
     echo "type-c-role-switch.sh <command>"
@@ -11,9 +14,38 @@ help_diag() {
     echo ""
 }
 
+if ! [ -f "$role_file_path" ]; then
+    echo "Couldn't find \"$role_file_path\""
+    echo "The role switching functionality isn't where we expect it to be."
+    exit 1
+fi
+
+if ! [ -d "$base_gpio_path" ]; then
+    echo "The gpio functionality isn't available through sysfs."
+    exit 2
+fi
+
+if ! [ -d "$base_gpio_path/gpio$cc_rd_en_pin" ]; then
+    echo "$cc_rd_en_pin" > "$base_gpio_path/export"
+    if ! [ -d "$base_gpio_path/gpio$cc_rd_en_pin" ]; then
+        echo "Couldn't get the sysfs directory for the gpio we wanted ($cc_rd_en_pin) exported."
+        exit 3
+    fi
+    echo "out" > "$base_gpio_path/gpio$cc_rd_en_pin/direction"
+fi
+
+if ! [ -d "$base_gpio_path/gpio$cc_rp_en_pin" ]; then
+    echo "$cc_rp_en_pin" > "$base_gpio_path/export"
+    if ! [ -d "$base_gpio_path/gpio$cc_rp_en_pin" ]; then
+        echo "Couldn't get the sysfs directory for the gpio we wanted ($cc_rp_en_pin) exported."
+        exit 4
+    fi
+    echo "out" > "$base_gpio_path/gpio$cc_rp_en_pin/direction"
+fi
+
 case $command in
-    "host") echo_value="host" ;;
-    "device") echo_value="none" ;;
+    "host") echo_value="host"; gpio_value="0" ;;
+    "device") echo_value="none"; gpio_value="1" ;;
     *) echo "Unrecognized argument"; help_diag; exit ;;
 esac
 
@@ -24,4 +56,15 @@ if [ "$new_role_value" = "$echo_value" ]; then
     echo "Successfully changed usb c role"
 else
     echo "Unable to change the usb c role"
+fi
+
+echo "$gpio_value" > "$base_gpio_path/gpio$cc_rd_en_pin/value"
+cc_rd_en_value=$(cat "$base_gpio_path/gpio$cc_rd_en_pin/value")
+echo "$gpio_value" > "$base_gpio_path/gpio$cc_rp_en_pin/value"
+cc_rp_en_value=$(cat "$base_gpio_path/gpio$cc_rp_en_pin/value")
+
+if [ "$gpio_value" = "$cc_rd_en_value" ] && [ "$gpio_value" = "$cc_rp_en_value" ]; then
+    echo "Successfully set the cc gpio pins"
+else
+    echo "Unable to change the cc gpio pins"
 fi
